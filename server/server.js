f67018498a0076102416ba68522fa00d50a74b71
Raul Mendez-Vasquez <rmendezvasquez@gmail.com>
2023-06-08 21:12:26 +0200

    configuration of the static

--- server/server.js
+++ server/server.js
@@ -28,7 +28,13 @@ app.use(urlencoded({ extended: false }));
 // Parse application/json
 app.use(json());
 
-app.use(express.static('public'));
+// Serve static files from the "dist" directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Serve the index.html file for all routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

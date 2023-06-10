de51dbbb676eb552fb505baafec1777fa3474fb9
Raul Mendez-Vasquez <rmendezvasquez@gmail.com>
2023-06-10 22:22:23 +0200

    fixing server issues related to the dist and main.js 3

--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,6 @@ import cors from 'cors';
 import { sendEmail } from '../nodemailer.js';
 import path from 'path';
 import bodyParser from 'body-parser';
-//import mime from 'mime';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const app = express();
@@ -21,45 +20,42 @@ app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Serve the static assets (CSS, images, JS)
-app.use(express.static(path.resolve(__dirname, 'dist')));
-
 // Define the MIME types for different file extensions
 const mimeTypes = {
-  '.js': 'application/javascript',
+  '.html': 'text/html',
   '.css': 'text/css',
+  '.js': 'application/javascript',
   '.jpg': 'image/jpeg',
   '.png': 'image/png',
   // Add more file extensions and corresponding MIME types as needed
 };
 
-// Set headers based on file extension
-app.use((req, res, next) => {
-  const filePath = path.join(__dirname, 'dist', req.path);
-  const mimeType = mimeTypes[path.extname(filePath)];
-  if (mimeType) {
-    res.setHeader('Content-Type', mimeType);
-  }
-  next();
-});
+// Serve the bundled JavaScript file
+app.use(express.static(path.resolve(__dirname, './dist')));
 
 // Serve the index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+  res.sendFile(path.resolve(__dirname, './dist/index.html'));
 });
 
-// Serve the bundled JavaScript file
-app.get('/assets/main.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist/assets/main.js'));
-});
+// Serve static assets with appropriate headers
+app.use(express.static(path.resolve(__dirname, './dist'), {
+  setHeaders: (res, filePath) => {
+    const fileExtension = path.extname(filePath);
+    const mimeType = mimeTypes[fileExtension];
+    if (mimeType) {
+      res.setHeader('Content-Type', mimeType);
+    }
+  },
+}));
 
 // Handle the sendEmail route
-app.post('/sendEmail', (req, res) => {
+app.post('/sendEmail', function (req, res) {
   sendEmail(req.body)
-    .then(() => {
+    .then(function () {
       res.sendStatus(200);
     })
-    .catch((error) => {
+    .catch(function (error) {
       console.error("EEEEEEE " + error);
       res.sendStatus(500);
     });
